3dae1ff5a0e32f7df8642d31f2a0d577e9789b1a
programthis <kylebachan@gmail.com>
2019-04-07 11:47:19 -0400

    Working on tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tag.create(name: "Events")
+Tag.create(name: "Programs")
+Tag.create(name: "Classes")
+Tag.create(name: "Misc")
