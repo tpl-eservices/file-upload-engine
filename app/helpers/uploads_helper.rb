6b310f4f235b4fd72f86250914d8e6193513ff0f
programthis <kylebachan@gmail.com>
2019-04-21 17:27:29 -0400

    Working on opening source file

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -2,4 +2,8 @@ module UploadsHelper
 	def bytes_to_megabytes(bytes)
 	    (bytes.to_f / 1000000).round(2)
 	end
+
+	def file_path(file)
+		ActiveStorage::Blob.service.send(:path_for, file.key)
+	end
 end
