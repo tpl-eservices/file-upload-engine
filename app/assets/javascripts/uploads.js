c778e286d1ccc1bae7b80bf25b51f231fecbe010
programthis <kylebachan@gmail.com>
2019-04-22 16:23:12 -0400

    Fixed turbolinks javascript

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -1,7 +1,9 @@
-$(function() {
-	//TO DO--add code to copy the event url based on the uploaded files
-	$('#upload_tag_list').tagsInput({
-		'height': 'auto',
-		'width': 'auto',
-	});
+
+
+$(document).on('turbolinks:load', function() {
+	  $('#upload_tag_list').tagsInput({
+			'height': 'auto',
+			'width': 'auto',
+		});
+
 });
