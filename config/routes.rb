950e3df362f971f18e5035494b15c89f13afe794
programthis <kylebachan@gmail.com>
2019-04-23 21:31:04 -0400

    Added authentication to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 	get 'welcome/index'
 
-	resources :uploads do
-	  member do
-	    delete :delete_image_attachment
-	  end
+	authenticate :user do
+		resources :uploads do
+		  member do
+		    delete :delete_image_attachment
+		  end
+		end
+		resources :tags
+		root 'uploads#new'
 	end
-	resources :tags
-
-	root 'uploads#new'
 end
