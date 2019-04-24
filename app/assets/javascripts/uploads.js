ce133c656beb31064c090d3b575966f8db6ce9d6
programthis <kylebachan@gmail.com>
2019-04-23 21:23:22 -0400

    User can now copy event url to clipboard

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -3,4 +3,28 @@ $(document).on('turbolinks:load', function() {
 		'height': 'auto',
 		'width': 'auto',
 	});
+
+	$(".copyEventUrl").click(function() {
+		let largeUrl = $(this).closest(".uploadDetailsContainer").prev().find(".imageContainer.large").attr("url"),
+			mediumUrl = $(this).closest(".uploadDetailsContainer").prev().find(".imageContainer.medium").attr("url"),
+			smallUrl = $(this).closest(".uploadDetailsContainer").prev().find(".imageContainer.small").attr("url"),
+			eventUrl = {
+				smallUrl: smallUrl,
+				mediumUrl: mediumUrl,
+				largeUrl: largeUrl
+			}
+		eventUrl = JSON.stringify(eventUrl);
+		copyToClipboard(eventUrl);
+		$(this).next().addClass("active");
+		$(this).next().html("Copied event url to clipboard.");
+	});
 });
+
+function copyToClipboard(text){
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
