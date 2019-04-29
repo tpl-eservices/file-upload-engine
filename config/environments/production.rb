f6ade6d606da7b8fa70c2abf08e0deea32124368
programthis <kylebachan@gmail.com>
2019-04-28 23:57:43 -0400

    Email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            '<%= Rails.application.credentials.dig(:email, :username) %>',
-    password:             '<%= Rails.application.credentials.dig(:email, :password) %>',
+    user_name:            '<%= Rails.application.credentials.email[:user_name]  %>',
+    password:             '<%= Rails.application.credentials.email[:password]  %>',
     authentication:       'plain',
     enable_starttls_auto: true }
 
