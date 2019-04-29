9af0833247f7448d28ee4c414c3d5f9dfadf8c44
programthis <kylebachan@gmail.com>
2019-04-29 18:27:41 -0400

    Working on email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: "https://serene-lake-43188.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
