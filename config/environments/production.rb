2ea3c8fe8e975fe47c069ac127d6a76589109c62
programthis <kylebachan@gmail.com>
2019-04-28 23:51:39 -0400

    Updated typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_options = { host: "https://serene-lake-43188.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://serene-lake-43188.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
