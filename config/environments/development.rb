1ed06cd51ddce30b71177979764959fafc784ec3
programthis <kylebachan@gmail.com>
2019-04-28 23:45:15 -0400

    Updated development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = { from: 'no-reply@gmail.com', host: "https://serene-lake-43188.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            '<%= Rails.application.credentials.dig(:email, :username) %>',
+    password:             '<%= Rails.application.credentials.dig(:email, :password) %>',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
