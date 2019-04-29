5b41a86ff76d92e5e3eec1ecc8f6544f08826294
programthis <kylebachan@gmail.com>
2019-04-28 23:31:54 -0400

    Working on mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = {from: 'no-reply@gmail.com'}
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
