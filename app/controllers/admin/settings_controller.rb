36e70794e839691730c066cdbd54950fcf21904c
programthis <kylebachan@gmail.com>
2019-05-16 19:14:43 -0400

    Added authenticate method for admin

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,4 +1,11 @@
 class Admin::SettingsController < ApplicationController
-  def index
-  end
+	before_action :authenticate_user!
+	before_action :authenticate_admin
+
+	def authenticate_admin
+		redirect_to '/', alert: "You are not authorized to access this page." unless current_user.admin?
+	end
+
+	def index
+	end
 end
