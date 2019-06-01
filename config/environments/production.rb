9c1d0f98a914222d20b9c24bc45c370933462f9c
programthis <kylebachan@gmail.com>
2019-06-01 18:56:13 -0400

    Updated mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: "https://serene-lake-43188.herokuapp.com" }
+  config.action_mailer.default_options = { from: "no-reply@file-engine.com", reply_to: "no-reply@file-engine.com" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
