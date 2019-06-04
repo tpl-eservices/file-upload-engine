8e01aba0e7b1babeddda8b6e9506b5f1cdb2d71e
programthis <kylebachan@gmail.com>
2019-06-03 21:20:11 -0400

    Changing host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'herokuapp.com',
     user_name:            Rails.application.credentials.email[:address],
     password:             Rails.application.credentials.email[:password],
     authentication:       'plain',
