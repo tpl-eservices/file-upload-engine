cae9e77b0d0926c8d11abac884b8ee88a79d428c
programthis <kylebachan@gmail.com>
2019-06-18 11:33:48 -0400

    Updated base url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "https://serene-lake-43188.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://upload.tpllabs.ca" }
   config.action_mailer.default_options = { from: "no-reply@fileuploadengine.com", reply_to: "no-reply@fileuploadengine.com" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
